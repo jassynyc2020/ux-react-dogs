af2745d27edd163172a306fe03d0f216de2f3ac9
Tyler Gary <tygary@gmail.com>
2020-06-25 10:58:37 -0700

    adding build

---
+++ build/precache-manifest.9a65593666a21a59aa8534e3d709685b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c8724a96b03f6868094339956126c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97686535b1a136fb6ed1",
+    "url": "/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "e3b7f73276b69b825274",
+    "url": "/static/js/2.1d268299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d268299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97686535b1a136fb6ed1",
+    "url": "/static/js/main.b6b24fe0.chunk.js"
+  },
+  {
+    "revision": "f22d304add47946b7ebd",
+    "url": "/static/js/runtime-main.d83dce69.js"
+  }
+]);
